0415283bf21143aa1610a2cb298349b295e0570a
Anum-09 <anumzaidi1994@gmail.com>
2024-02-18 14:30:36 -0500

    created a few files, modified some

---
+++ src/Checkout.js
@@ -0,0 +1,13 @@
+import React from 'react'
+// import Header from './Header'
+
+function Checkout() {
+  return (
+    <div>
+  
+      <h1>Checkout</h1>
+    </div>
+  )
+}
+
+export default Checkout
---
+++ src/Homepage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from "./Header"
+
+const Homepage = () => {
+  return (
+    <div>
+      <Header />
+      <h1>HOME PAGE!!!</h1>
+    </div>
+  )
+}
+
+export default Homepage
