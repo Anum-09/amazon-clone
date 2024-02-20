811ea06057370ce5289d776fc7d44cdc750e6c93
Anum-09 <anumzaidi1994@gmail.com>
2024-02-19 23:18:06 -0500

    WIP

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,10 +1,10 @@
 import React from 'react'
-// import Header from './Header'
+import Header from './Header'
 
 function Checkout() {
   return (
     <div>
-  
+      <Header />
       <h1>Checkout</h1>
     </div>
   )
--- src/Homepage.js
+++ src/Homepage.js
@@ -1,11 +1,27 @@
 import React from 'react'
 import Header from "./Header"
+import Product from "./Product"
+import "./Homepage.css"
 
 const Homepage = () => {
   return (
-    <div>
+    <div className="home">
       <Header />
-      <h1>HOME PAGE!!!</h1>
+      <img 
+          className="home__image"
+          src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg"
+          alt=""
+      />
+
+       {/* product.id , product.title, product.image, product.price, product.rating  */}
+      <Product 
+         id="1234"
+         title="Amazon basic USB 2.0 A-Male to Micro B cable"
+         price={17.99}
+         rating={4}
+         image="https://m.media-amazon.com/images/I/71AB2RWLveL._AC_UL640_FMwebp_QL65_.jpg"
+      />
+
     </div>
   )
 }
